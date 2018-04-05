43949b45bf3872f374104956a3c9faed03d5237a
Matthew Bolles <mattbolles12@gmail.com>
2018-04-04 23:37:10 -0400

    Added new enemy type

---
+++ Ceres/Assets/Scripts/FlyingCreeper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCreeper : MonoBehaviour {
+
+	public float speed;
+	public Transform player;
+	public Transform startingPoint;
+
+	bool playerInRange;
+
+	void FixedUpdate () {
+		if(playerInRange){
+			Attack();
+		}
+		else{
+			StopAttackingAndReturn();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag == "Player"){
+			Debug.Log("Player Detected");
+			playerInRange = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.tag == "Player"){
+			playerInRange = false;
+		}
+	}
+	
+	void Attack(){
+		Debug.Log("Attacking");
+		gameObject.transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+	}
+
+	void StopAttackingAndReturn(){
+		Debug.Log("Returning");
+		gameObject.transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);	
+	}
+}
